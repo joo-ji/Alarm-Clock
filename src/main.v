7285bd0729f04302820c0556bcb82d42ffb28820
george <37781509+jyji@users.noreply.github.com>
2018-11-15 00:01:37 -0800

    update main.v

--- src/main.v
+++ src/main.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/27/2018 01:15:42 PM
-// Design Name: 
-// Module Name: main
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module main(
     input clk, 
